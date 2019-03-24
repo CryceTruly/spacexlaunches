f218c15e5958f6fec1fdfd5b56c686468c9721aa
crycetruly <crycetruly@gmail.com>
2019-03-25 00:14:17 +0300

    ft(powerClient): display launches with react

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express=require('express');
 const graphqlHTTP=require('express-graphql');
+const cors=require('cors');
 const app=express();
+
+
+
+//cors middleware
+app.use(cors())
+
+
 const schema=require('./schema.js');
 app.use('/graphql',graphqlHTTP({
 schema,
 graphiql:true
 }));
 
+
+//port
 const PORT=process.env.PORT||5000;
 app.listen(PORT,()=>console.log('Server running'))
